9bf94950a59d2d8cabd4d28a9aebee720a2ee981
Red and Green <62441426+RGGH@users.noreply.github.com>
2021-08-11 14:52:37 +0100

    Update socket1.py

--- part1/socket1.py
+++ part1/socket1.py
@@ -15,7 +15,7 @@ PORT = 9999
 
 def main():
     # create server object and listen on specified port
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #AF_INET = ipv4, SOCK_STREAM = tcp
     server.bind((IP, PORT))
     server.listen(5)
 
@@ -25,7 +25,7 @@ def main():
         # wait for an incomng connection
         client, address = server.accept() 
         print(f'[*] Accepted connection from {address[0]}:{address[1]}')
-        
+
         client_handler = threading.Thread(target=handle_client, args=(client,))
         client_handler.start()
 
@@ -37,3 +37,6 @@ def handle_client(client_socket):
 
 if __name__ == '__main__':
     main() 
+
+# test with a 2nd shell, type 'telnet localhost 9999' and then on next line add text
+# also you can get similar with results with netcat as a listener, but this may be removed on secure machines
